487c7248486e7b574ec2a0d51e1d745b65034c16
Qoqnus master <mahisokhary@gmail.com>
2026-07-26 09:49:06 +0330

    qolman: Created internal header qolman

---
+++ qolman/qolman.internal.h
@@ -0,0 +1,15 @@
+#ifndef _qolman_qolman_internal_h_
+#define _qolman_qolman_internal_h_
+
+#include <qolman/qolman.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
