8a9927d8cf3635ecc2182ae8f1599c72b3b92d4d
Qoqnus master <mahisokhary@gmail.com>
2026-08-15 11:36:03 +0330

    qolman/level: Created internal header level

---
+++ qolman/level/level.internal.h
@@ -0,0 +1,15 @@
+#ifndef _qolman_level_level_internal_h_
+#define _qolman_level_level_internal_h_
+
+#include <qolman/qolman.internal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
