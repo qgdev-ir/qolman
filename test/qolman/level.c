608488e6b3838c903b9c72ab4a7066ff683eaa8f
Qoqnus master <mahisokhary@gmail.com>
2026-08-22 13:32:45 +0330

    test/qolman: Created test group level

---
+++ test/qolman/level.c
@@ -0,0 +1,7 @@
+#include <test/test.h>
+
+bool test_qolman_level() {
+	bool success = 1;
+	return success;
+}
+
--- test/qolman/qolman.c
+++ test/qolman/qolman.c
@@ -1,7 +1,10 @@
 #include <test/test.h>
 
+extern bool test_qolman_level();
+
 bool test_qolman() {
 	bool success = 1;
+	success &= test_qolman_level();
 	return success;
 }
 
