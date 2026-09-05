657394522b05616f9a1a3a33a78f4150b59491ea
Qoqnus master <mahisokhary@gmail.com>
2026-09-05 10:24:46 +0330

    qolman: Initialized package record (GitHub #41)
    qolman/record:
    - Created header record
    - Created internal header record
    - Created source record
    
    qolman:
    - Included header record/record in header qolman

---
+++ qolman/record/record.h
@@ -0,0 +1,13 @@
+#ifndef _qolman_record_record_h_
+#define _qolman_record_record_h_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ qolman/record/record.internal.h
@@ -0,0 +1,15 @@
+#ifndef _qolman_record_record_internal_h_
+#define _qolman_record_record_internal_h_
+
+#include <qolman/qolman.internal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
