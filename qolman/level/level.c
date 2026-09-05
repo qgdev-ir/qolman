1a3ed9fee4a405b86cc4ca3427896a3345d01e6e
Qoqnus master <mahisokhary@gmail.com>
2026-09-05 06:21:41 +0330

    qolman/level: Created internal constant array containing default levels

--- qolman/level/level.c
+++ qolman/level/level.c
@@ -1,2 +1,18 @@
 #include "level.internal.h"
 
+/*
+ * This array should always be sorted by importance
+ * And no skipped importance
+ * Example:
+ * Wrong: -2 -3 0 (not sorted)
+ * Wrong: -2 0 1 2 (-1 is skipped)
+ * Correct: -1 0 1 2
+ */
+const struct qolman_level qolman_level_defaults[] = {
+	{ -2, "trace" },
+	{ -1, "debug" },
+	{ 0, "info" },
+	{ 1, "warning" },
+	{ 2, "error" },
+};
+
