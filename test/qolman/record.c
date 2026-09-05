219178b4af06f8e716d8724a807d35e9e3776b48
Qoqnus master <mahisokhary@gmail.com>
2026-09-05 13:10:34 +0330

    test/qolman/record: Added test for functions qolman_record_{create,destroy}

--- test/qolman/record.c
+++ test/qolman/record.c
@@ -1,8 +1,21 @@
 #include <test/test.h>
 #include <qolman/record/record.internal.h>
 
+bool test_qolman_record_create_destroy() {
+	test_run_log("qolman_record_create_destroy");
+	struct qolman_level level = { 2147483647, "godlevel" };
+	qolman_record_t record;
+
+	bool success = 1;
+	success &= qolman_record_create(&record, 1676385060, &level, "phoenix", NULL) == QOLMAN_RESULT_OK;
+	success &= qolman_record_destroy(record) == QOLMAN_RESULT_OK;
+	test_result_log(success);
+	return success;
+}
+
 bool test_qolman_record() {
 	bool success = 1;
+	success &= test_qolman_record_create_destroy();
 	return success;
 }
 
