13d87841d44c91c0c8c46dee89c2c911af556ed7
Qoqnus master <mahisokhary@gmail.com>
2026-09-05 10:28:06 +0330

    test/qolman: Created test group record (GitHub #43)

--- test/qolman/qolman.c
+++ test/qolman/qolman.c
@@ -1,10 +1,12 @@
 #include <test/test.h>
 
 extern bool test_qolman_level();
+extern bool test_qolman_record();
 
 bool test_qolman() {
 	bool success = 1;
 	success &= test_qolman_level();
+	success &= test_qolman_record();
 	return success;
 }
 
