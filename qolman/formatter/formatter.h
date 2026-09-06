9867371bf401768aada47f79b71069a2ab8c1b4b
Qoqnus master <mahisokhary@gmail.com>
2026-09-06 14:18:20 +0330

    qolman/formatter: Created typedef qolman_formatter_t

--- qolman/formatter/formatter.h
+++ qolman/formatter/formatter.h
@@ -6,6 +6,12 @@
 extern "C" {
 #endif
 
+/*
+ * A function that take a record and format it for output to file, stdout, ...
+ * Returned buffer must be freeable by std free function
+ */
+typedef char* (*qolman_formatter_t)(qolman_record_t *record);
+
 #ifdef __cplusplus
 }
 #endif
