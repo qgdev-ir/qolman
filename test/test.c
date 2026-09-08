220a5e23f6bf40844da0d9334d1eafb279c70310
Qoqnus master <mahisokhary@gmail.com>
2026-09-08 12:16:29 +0330

    test: Set  timezone to make tests predictable in diffrent systems with diffrent time zones

--- test/test.c
+++ test/test.c
@@ -1,8 +1,11 @@
 #include "test.h"
+#include <stdlib.h>
 
 extern bool test_qolman();
 
 int main() {
+	setenv("TZ", "Asia/Tehran", 1); // To get predictable times in systems with diffrent time zones
+
 	bool success = 1;
 	success &= test_qolman();
 	return !success;
