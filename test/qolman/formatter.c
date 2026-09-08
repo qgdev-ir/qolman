3fcdf4d41cc43766bfb680e56790f2a6c48aa0d6
Qoqnus master <mahisokhary@gmail.com>
2026-09-08 11:31:10 +0330

    test/qolman: Created test group formatter

---
+++ test/qolman/formatter.c
@@ -0,0 +1,8 @@
+#include <test/test.h>
+#include <qolman/qolman.internal.h>
+
+bool test_qolman_formatter() {
+	bool success = 1;
+	return success;
+}
+
--- test/qolman/qolman.c
+++ test/qolman/qolman.c
@@ -2,11 +2,13 @@
 
 extern bool test_qolman_level();
 extern bool test_qolman_record();
+extern bool test_qolman_formatter();
 
 bool test_qolman() {
 	bool success = 1;
 	success &= test_qolman_level();
 	success &= test_qolman_record();
+	success &= test_qolman_formatter();
 	return success;
 }
 
