094cfad69072d01b78023d07ce4ff2695e4c9bce
Qoqnus master <mahisokhary@gmail.com>
2026-09-08 11:35:22 +0330

    test/qolman/formatter: Added test for function qolman_formatter_text

--- test/qolman/formatter.c
+++ test/qolman/formatter.c
@@ -1,8 +1,39 @@
 #include <test/test.h>
 #include <qolman/qolman.internal.h>
 
+bool test_qolman_formatter_text() {
+	test_run_log("qolman_formatter_text");
+	struct qolman_level level = { 2147483647, "godlevel" };
+	struct qolman_record record = { 1676385060, &level, "phoenix", "some details\nsome more details\n" };
+	char *excpected = "[2023-02-14 18:01:00] [godlevel] phoenix\n\tsome details\n\tsome more details\n";
+	char *excpected2 = "[2023-02-14 18:01:00] [godlevel] phoenix\n";
+	char *formatted;
+
+	bool success = 1;
+
+	formatted = qolman_formatter_text(&record);
+	success &= strcmp(excpected, formatted) == 0;
+	free(formatted);
+
+	// Should ignore description if empty
+	record.description = "";
+	formatted = qolman_formatter_text(&record);
+	success &= strcmp(excpected2, formatted) == 0;
+	free(formatted);
+
+	// Should ignore description if null
+	record.description = NULL;
+	formatted = qolman_formatter_text(&record);
+	success &= strcmp(excpected2, formatted) == 0;
+	free(formatted);
+
+	test_result_log(success);
+	return success;
+}
+
 bool test_qolman_formatter() {
 	bool success = 1;
+	success &= test_qolman_formatter_text();
 	return success;
 }
 
