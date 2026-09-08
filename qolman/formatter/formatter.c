d1a0a35dfcdc7601e93f0081a66f5c8ee531516e
Qoqnus master <mahisokhary@gmail.com>
2026-09-08 11:34:55 +0330

    qolman/formatter: Added internal inline function _buffer_write to write formatted in internal struct buffer

--- qolman/formatter/formatter.c
+++ qolman/formatter/formatter.c
@@ -6,3 +6,21 @@ struct buffer {
 	size_t l;
 };
 
+static inline void _buffer_write(struct buffer *b, const char *fmt, ...) {
+	va_list ap, ap2;
+	va_start(ap, fmt);
+	va_copy(ap2, ap);
+
+	size_t len = vsnprintf(NULL, 0, fmt, ap2) + 1;
+	va_end(ap2);
+
+	if (len > (b->s - b->l)) {
+		b->s += len;
+		b->b = realloc(b->b, b->s);
+	}
+
+	int r = vsnprintf(b->b + b->l, len, fmt, ap);
+	b->l += len - 1;	// -1 so next write will write over the null terminator
+	va_end(ap);
+}
+
