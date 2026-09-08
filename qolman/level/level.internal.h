ef87cc45c4fd5a5ac1a415c0c4a727ee3d60b7e0
Qoqnus master <mahisokhary@gmail.com>
2026-09-08 10:55:42 +0330

    qolman: Removed includion of internal header qolman from other internal headers

--- qolman/level/level.internal.h
+++ qolman/level/level.internal.h
@@ -1,8 +1,6 @@
 #ifndef _qolman_level_level_internal_h_
 #define _qolman_level_level_internal_h_
 
-#include <qolman/qolman.internal.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
--- qolman/record/record.internal.h
+++ qolman/record/record.internal.h
@@ -1,8 +1,6 @@
 #ifndef _qolman_record_record_internal_h_
 #define _qolman_record_record_internal_h_
 
-#include <qolman/qolman.internal.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
