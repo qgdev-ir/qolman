5124cf5e0c358bea6a0be692c53635fc22acbe6e
Qoqnus master <mahisokhary@gmail.com>
2026-09-09 13:41:25 +0330

    test/qolman: Created test group handler

---
+++ test/qolman/handler.c
@@ -0,0 +1,7 @@
+#include <test/test.h>
+
+bool test_qolman_handler() {
+	bool success = 1;
+	return success;
+}
+
--- test/qolman/qolman.c
+++ test/qolman/qolman.c
@@ -3,12 +3,14 @@
 extern bool test_qolman_level();
 extern bool test_qolman_record();
 extern bool test_qolman_formatter();
+extern bool test_qolman_handler();
 
 bool test_qolman() {
 	bool success = 1;
 	success &= test_qolman_level();
 	success &= test_qolman_record();
 	success &= test_qolman_formatter();
+	success &= test_qolman_handler();
 	return success;
 }
 
