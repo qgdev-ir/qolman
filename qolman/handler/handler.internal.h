f19b24568dc898ac02f763f279a3491b8b774c7c
Qoqnus master <mahisokhary@gmail.com>
2026-09-11 12:21:37 +0330

    qolman/handler: Created internal struct qolman_handler

--- qolman/handler/handler.internal.h
+++ qolman/handler/handler.internal.h
@@ -5,6 +5,17 @@
 extern "C" {
 #endif
 
+/*
+ * A log handler
+ */
+struct qolman_handler {
+	qolman_handler_function_handle handle;
+	qolman_handler_function_destroy destroy;
+	qolman_level_t level;
+	qolman_formatter_t formatter;
+	char data[];
+};
+
 #ifdef __cplusplus
 }
 #endif
