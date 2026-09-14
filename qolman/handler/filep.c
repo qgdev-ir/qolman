bb4183e55c4491a007d3677073a1bf82e855665b
Qoqnus master <mahisokhary@gmail.com>
2026-09-14 15:37:24 +0330

    qolman/handler/filep: Added internal function _qolman_handler_filep_destroy

--- qolman/handler/filep.c
+++ qolman/handler/filep.c
@@ -10,3 +10,8 @@ static void _qolman_handler_filep_handle(qolman_handler_t h, char *log) {
 	fprintf(d->fp, "%s", log);
 }
 
+static void _qolman_handler_filep_destroy(qolman_handler_t h) {
+	struct data *d = (struct data *) h->data;
+	if (d->close) fclose(d->fp);
+}
+
