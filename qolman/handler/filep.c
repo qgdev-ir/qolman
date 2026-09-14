3f18e7c3c128e942adc674563fb3d1a2d600bbbb
Qoqnus master <mahisokhary@gmail.com>
2026-09-14 15:37:18 +0330

    qolman/handler/filep: Added internal function _qolman_handler_filep_handle

--- qolman/handler/filep.c
+++ qolman/handler/filep.c
@@ -5,3 +5,8 @@ struct data {
 	bool close;
 };
 
+static void _qolman_handler_filep_handle(qolman_handler_t h, char *log) {
+	struct data *d = (struct data *) h->data;
+	fprintf(d->fp, "%s", log);
+}
+
