49f4eb0b2fe85d482a11b82f857a90db3a717dd9
Qoqnus master <mahisokhary@gmail.com>
2026-09-16 11:30:00 +0330

    qolman/manager: Addedd function qolman_manager_create

--- qolman/manager/manager.c
+++ qolman/manager/manager.c
@@ -1,2 +1,12 @@
 #include <qolman/qolman.internal.h>
 
+qolman_result_t qolman_manager_create(qolman_manager_t *manager, qolman_formatter_t formatter) {
+	qolman_manager_t m = malloc(sizeof(struct qolman_manager));
+	m->level = NULL;
+	m->formatter = formatter;
+	m->handlers = NULL;
+	m->handlers_length = 0;
+	*manager = m;
+	return QOLMAN_RESULT_OK;
+}
+
--- qolman/manager/manager.h
+++ qolman/manager/manager.h
@@ -10,6 +10,11 @@ extern "C" {
  */
 typedef struct qolman_manager *qolman_manager_t;
 
+/*
+ * Creates a log manager
+ */
+qolman_result_t qolman_manager_create(qolman_manager_t *manager, qolman_formatter_t formatter);
+
 #ifdef __cplusplus
 }
 #endif
